87938c3b5ef8a0874fc56be24cc367b6e35d9d11
Shobha Chejara <shobha.chejara@gmail.com>
2024-09-21 11:44:14 -0500

    Created getList function to get list items

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,15 @@
 console.log('JS is sourced!');
+
+function getList(){
+    console.log( 'in getList' );
+    axios({
+      method: 'GET',
+      url: '/todos'
+    }).then(function(response) {
+      console.log('getList() response', response.data);
+      //renderKoalas(response.data);
+    }).catch(function(error){
+      console.log('error in GET', error);
+    });
+}
+getList();
--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -1,6 +1,18 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
 
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "todos"' ;
+    pool.query(queryText).then(result => {
+      // Sends back the results in an object
+      res.send(result.rows);
+    })
+    .catch(error => {
+      console.log('error getting books', error);
+      res.sendStatus(500);
+    });
+  });
+
 
 
 
