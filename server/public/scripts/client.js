340bc3efc3ae36eb0f4d1d60302673c62050877a
Shobha Chejara <shobha.chejara@gmail.com>
2024-09-21 13:03:11 -0500

    completed render part

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,9 +7,37 @@ function getList(){
       url: '/todos'
     }).then(function(response) {
       console.log('getList() response', response.data);
-      //renderKoalas(response.data);
+      renderToDom(response.data);
     }).catch(function(error){
       console.log('error in GET', error);
     });
 }
+
+function saveList(event){
+    event.preventDeafult();
+let todos = {};
+todos.toDoText = document.getElementById('').value
+
+}
+
+
+function renderToDom(todos){
+    let addItem = document.getElementById("addItem");
+    addItem.innerHTML = ''
+    for(let property of todos){
+
+        addItem.innerHTML += 
+        `
+        <tr>
+        <td>${property.text}</td>,        
+        <td>${property.isComplete}</td>
+        </tr>
+        `      
+    }
+}
+
+    
+
+
+
 getList();
